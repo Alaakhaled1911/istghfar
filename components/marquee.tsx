f563d62c4253296a56065c2a3fab6260dd26a981
Your Name <alaa.khaled_a108@fci.kfs.edu.eg>
2025-12-13 00:51:31 +0200

    Enhance duration

--- components/marquee.tsx
+++ components/marquee.tsx
@@ -8,7 +8,7 @@ export function Marquee() {
       <motion.div
         className="whitespace-nowrap inline-flex"
         animate={{ x: ["0%", "50%"] }}
-        transition={{ duration: 15, repeat: Infinity, ease: "linear" }}
+        transition={{ duration: 25, repeat: Infinity, ease: "linear" }}
       >
         <p className="text-lg font-bold text-green-800 px-6 inline-block">
           صدقه جاريه لامي حبيبتي هاله محمد بارك الله ف عمرها وصحتها وحفظها من كل سوء وجعلها من أهل رحمته...
